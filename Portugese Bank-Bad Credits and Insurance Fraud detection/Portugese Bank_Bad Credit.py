080d7c26acdd1e5615aad6da663f653bb513f4a3
sowmya2790 <sowmya2790@gmail.com>
2021-05-12 12:28:45 -0700

    Add files via upload

---
+++ Portugese Bank-Bad Credits and Insurance Fraud detection/Portugese Bank_Bad Credit.py
@@ -0,0 +1,222 @@
+# Commented out IPython magic to ensure Python compatibility.
+import pandas as pd
+from sklearn.preprocessing import label_binarize
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.preprocessing import OneHotEncoder
+from sklearn import metrics #Import scikit-learn metrics module for accuracy calculation
+
+import warnings
+warnings.filterwarnings("ignore")
+#Please ignore the warnings with version change
+
+from google.colab import drive
+drive.mount('/gdrive')
+#Change current working directory to gdrive
+# %cd /gdrive
+
+trainfile = r'/gdrive/My Drive/PortugeseBankData-TRAIN.csv'
+trainData = pd.read_csv(trainfile) #creates a dataframe
+testfile = r'/gdrive/My Drive/PortugeseBankData-TEST.csv'
+testData = pd.read_csv(testfile)  #creates a dataframe
+
+print(trainData.shape)
+print(testData.shape)
+
+trainData.info()
+
+testData.info()
+
+#To get list of names of all Columns from a dataframe
+TrainCols = list(trainData.columns.values)
+TestCols = list(testData.columns.values)
+print(TrainCols)
+print(TestCols)
+
+# Seperate Target column from Train Data
+Xtrain = trainData[TrainCols[0:len(TrainCols)-1]].copy()
+Ytrain = trainData[['y']].copy()
+print("Train Set shape:")
+print(Xtrain.shape)
+print(Ytrain.shape)
+Xtest = testData[TestCols[0:len(TestCols)-1]].copy()
+Ytest = testData[['y']].copy()
+print("Test Set shape:")
+print(Xtest.shape)
+print(Ytest.shape)
+
+# OneHotEncoding on Train (fit & transform)
+# OneHotEncoding is to be done on Categorical variables.
+categoricalFeatures=list(Xtrain.columns.values)
+ohe = OneHotEncoder(handle_unknown='ignore',sparse=False)
+Xcat = pd.DataFrame(ohe.fit_transform(Xtrain[categoricalFeatures]),columns=ohe.get_feature_names(),index=Xtrain.index)
+Xtrain = pd.concat([Xtrain,Xcat],axis=1)
+Xtrain.drop(labels=categoricalFeatures,axis=1,inplace=True)
+Xtrain.sample(5)
+
+# OneHotEncoding on Test (only transform)
+# OneHotEncoding is to be done on Categorical variables.
+Xcat = pd.DataFrame(ohe.transform(Xtest[categoricalFeatures]),columns=ohe.get_feature_names(),index=Xtest.index)
+Xtest = pd.concat([Xtest,Xcat],axis=1)
+Xtest.drop(labels=categoricalFeatures,axis=1,inplace=True)
+Xtest.sample(5)
+
+dt = DecisionTreeClassifier()
+dt.fit(Xtrain, Ytrain)
+
+rf = RandomForestClassifier()
+rf.fit(Xtrain, Ytrain)
+
+X_Pred = dt.predict(Xtest)
+XPred = dt.predict(Xtrain)
+
+
+#Model Accuracy
+print("Train Accuracy:", metrics.accuracy_score(Ytrain,XPred))
+print("Test Accuracy:", metrics.accuracy_score(Ytest,X_Pred))
+print("Confusion Matrix for Decision Tree:")
+print(confusion_matrix(Ytest,X_Pred))
+print("Max Depth",dt.get_depth())
+print("Leaf",dt.get_n_leaves())
+print('Printing the precision and recall, among other metrics')
+print(metrics.classification_report(Ytest, X_Pred))
+
+X_Pred1 = rf.predict(Xtest)
+XPred1 = rf.predict(Xtrain)
+#Model Accuracy
+print("Train Accuracy:", metrics.accuracy_score(Ytrain,XPred1))
+print("Test Accuracy:", metrics.accuracy_score(Ytest,X_Pred1))
+print("Confusion Matrix for Decision Tree:")
+print(confusion_matrix(Ytest,X_Pred1))
+print('Printing the precision and recall, among other metrics')
+print(metrics.classification_report(Ytest, X_Pred1))
+
+"""Decision Tree: Random & Grid Search"""
+
+#Hyperparameter tuning done for decision tree classifier
+
+#RANDOM SEARCH--------------------------------------------
+
+import time
+start_time = time.time()
+
+print("RandomizedSearchCV-Decision tree")
+parameters={'min_samples_leaf' : range(10,300,10),'max_depth': 
+            range(5,30,2),'criterion':['gini','entropy']}
+dt_random = RandomizedSearchCV(dt,parameters,n_iter=25,cv=5)
+dt_random.fit(Xtrain, Ytrain)
+grid_parm=dt_random.best_params_
+print(grid_parm)
+print("accuracy Score for Decision Tree:{0:6f}".
+      format(dt_random.score(Xtest,Ytest)))
+
+print("--- %s seconds ---" % (time.time() - start_time))
+
+#GRID SEARCH----------------------------------------
+
+import time
+start_time = time.time()
+
+print("GridSearchCV-Decision tree")
+dt_grid = GridSearchCV(dt,parameters)
+dt_grid.fit(Xtrain, Ytrain)
+grid_parm1=dt_grid.best_params_
+print(grid_parm1)
+print("accuracy Score for Decision Tree:{0:6f}".
+      format(dt_grid.score(Xtest,Ytest)))
+
+print("--- %s seconds ---" % (time.time() - start_time))
+
+#Using the parameters obtained from HyperParameterTuning in the DecisionTreeClassifier 
+dtRand = DecisionTreeClassifier(**grid_parm)
+dtGrid = DecisionTreeClassifier(**grid_parm1)
+
+dtRand.fit(Xtrain,Ytrain)
+dtRand_predict = dtRand.predict(Xtest)
+dtGrid.fit(Xtrain,Ytrain)
+dtGrid_predict = dtGrid.predict(Xtest)
+
+# Accuracy for Decision Tree using Random Search CV for Hyperparameter Tuning
+
+print("Test Accuracy:", metrics.accuracy_score(Ytest,dtRand_predict))
+print("Confusion Matrix for Decision Tree:")
+print(confusion_matrix(Ytest,dtRand_predict))
+print('Printing the precision and recall, among other metrics')
+print(metrics.classification_report(Ytest, dtRand_predict))
+clf_cv_score = cross_val_score(dtRand, Xtrain, Ytrain, cv=5, scoring="balanced_accuracy")
+print(clf_cv_score)
+
+# Accuracy for Decision Tree using Grid Search for Hyperparameter Tuning
+
+print("Test Accuracy:", metrics.accuracy_score(Ytest,dtGrid_predict))
+print("Confusion Matrix for Decision Tree:")
+print(confusion_matrix(Ytest,dtGrid_predict))
+print('Printing the precision and recall, among other metrics')
+
+"""Random Forest: Random & Grid Search"""
+
+#Hyperparameter tuning done for random forest classifier
+
+#RANDOM SEARCH--------------------------------------------
+
+import time
+start_time = time.time()
+
+print("RandomizedSearchCV-Random forest")
+rand_parameters={'min_samples_leaf' : range(10,100,10),'max_depth': 
+            range(1,10,2),'max_features':[10,20,30],'n_estimators':[20,30,40]}
+rf_random = RandomizedSearchCV(rf,rand_parameters,n_iter=25,cv=5)
+rf_random.fit(Xtrain, Ytrain)
+grid_parm=rf_random.best_params_
+print(grid_parm)
+print("accuracy Score for Decision Tree:{0:6f}".
+      format(rf_random.score(Xtest,Ytest)))
+
+print("--- %s seconds ---" % (time.time() - start_time))
+
+import time
+start_time = time.time()
+
+print("GridSearchCV-Random Forest")
+rf_grid = GridSearchCV(rf,rand_parameters)
+rf_grid.fit(Xtrain, Ytrain)
+grid_parm1=rf_grid.best_params_
+print(grid_parm1)
+print("accuracy Score for Decision Tree:{0:6f}".
+      format(rf_grid.score(Xtest,Ytest)))
+
+print("--- %s seconds ---" % (time.time() - start_time))
+
+#Using the parameters obtained from HyperParameterTuning in the RandomForestClassifier 
+rfRand = RandomForestClassifier(**grid_parm)
+rfGrid = RandomForestClassifier(**grid_parm1)
+
+rfRand.fit(Xtrain,Ytrain)
+rfRand_predict = rfRand.predict(Xtest)
+rfGrid.fit(Xtrain,Ytrain)
+rfGrid_predict = rfGrid.predict(Xtest)
+
+# Accuracy for Random Forest using Random Search CV for Hyperparameter Tuning
+
+print("Test Accuracy:", metrics.accuracy_score(Ytest,rfRand_predict))
+print("Confusion Matrix for Decision Tree:")
+print(confusion_matrix(Ytest,rfRand_predict))
+print('Printing the precision and recall, among other metrics')
+print(metrics.classification_report(Ytest, rfRand_predict))
+clf_cv_score = cross_val_score(rfRand, Xtrain, Ytrain, cv=5, scoring="balanced_accuracy")
+print(clf_cv_score)
+
+# Accuracy for Random Forest using Grid Search for Hyperparameter Tuning
+
+print("Test Accuracy:", metrics.accuracy_score(Ytest,rfGrid_predict))
+print("Confusion Matrix for Decision Tree:")
+print(confusion_matrix(Ytest,rfGrid_predict))
+print('Printing the precision and recall, among other metrics')
+print(metrics.classification_report(Ytest, rfGrid_predict))
